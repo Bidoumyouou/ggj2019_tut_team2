483be0ebe0af3e20577697ccba5ee90b668250de
Bidou_Google <bidoumyouou@gmail.com>
2019-01-27 14:06:19 +0900

    リザルト文字列条件の計算

--- Assets/ResultControl.cs
+++ Assets/ResultControl.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class ResultWord {
+    [Tooltip("この数値以上なら")]public int treshold;
+    [Tooltip("この文字列になる")]public string str;
+}
+
+
 public class ResultControl : MonoBehaviour
 {
+
+    GameMgr gameMgr;
     public Image fadeTex;
 
     public GameObject player;
@@ -16,19 +25,52 @@ public class ResultControl : MonoBehaviour
     private bool fadeOut;
     private bool fadeIn;
 
+    [SerializeField] public List<ResultWord> resultWordListLife;
+    [SerializeField] public List<ResultWord> resultWordListHappiness;
+    [SerializeField] public List<ResultWord> resultWordListHome;
+
     int result_state = 0;//0..フェードインする 1.プレイヤーを消す 2.フェードアウトする 3.. アニメーション開始 
+
+    public string[] resultStr = new string[3];
     // Start is called before the first frame update
     void Start()
     {
+        gameMgr = GameObject.Find("GameMgr").GetComponent<GameMgr>();
         //fadeIn = true;
     }
 
     // Update is called once per frame
-
+    void CalcResultText()
+    {
+        foreach (ResultWord word in resultWordListLife)
+        {
+            if (word.treshold <= gameMgr.scoreLife)
+            {
+                resultStr[0] = word.str;
+            }
+        }
+        foreach (ResultWord word in resultWordListHappiness)
+        {
+            if (word.treshold <= gameMgr.scoreHappiness)
+            {
+                resultStr[1] = word.str;
+            }
+        }
+        foreach (ResultWord word in resultWordListHome)
+        {
+            if (word.treshold <= gameMgr.scoreHome)
+            {
+                resultStr[2] = word.str;
+            }
+        }
+    }
 
 
     void Update()
     {
+        //リザルトテキストの編集
+        CalcResultText();
+
         //phadeインへの遷移
         if(GameMgr.state == GameState.result)
         {
